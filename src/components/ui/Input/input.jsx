6fc5120aa37511587c248b6f99f7385b7ae921e7
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-03-28 13:44:01 -0300

    [#7] create and style input

---
+++ src/components/ui/Input/input.jsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+import { cn } from "../../../lib/utils"
+
+const Input = React.forwardRef(({ className, type, label, ...props }, ref) => {
+  return (
+    <div>
+      {label && <label className="font-semibold">{label}</label>}
+      <input
+        type={type}
+        className={cn(
+          "flex h-8 w-full text-primary rounded-sm border-2 border-[#A3A3A3] bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus:border-primary disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props} 
+      />
+    </div>
+  );
+})
+
+Input.displayName = "Input"
+
+export { Input }
--- src/screens/TestComponents.jsx
+++ src/screens/TestComponents.jsx
@@ -1,9 +1,12 @@
 import { Button } from '@/components/ui/button';
+import { Input } from '../components/ui/Input/input';
 
 export default function TestComponents() {
   return (
     <div>
       <Button>Teste!</Button>
+
+      <Input placeholder="teste" type="password" label="Teste Label" />
     </div>
   );
 }
