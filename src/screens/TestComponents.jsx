33e4abafb41467bc22b3e78ce8b0093237db3ee4
gabrielfel1x <gabrielfelxx@gmail.com>
2024-03-28 11:34:42 -0300

    [#6] Create RegisterHeader Component

--- src/screens/TestComponents.jsx
+++ src/screens/TestComponents.jsx
@@ -1,9 +1,11 @@
 import { Button } from '@/components/ui/button';
+import { HeaderRegister } from '@/components/HeaderRegister';
 
 export default function TestComponents() {
   return (
     <div>
       <Button>Teste!</Button>
+      <HeaderRegister />
     </div>
   );
 }
