9dd390fdfe26ebbe69a6d9602b350ccb703e41f3
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-03-29 10:46:20 -0300

    [#85] add the sectionDescription component

---
+++ src/components/SectionDescription/index.jsx
@@ -0,0 +1,8 @@
+export function SectionDescription({ title, subTitle }) {
+  return (
+    <div className="text-center">
+      <h1 className="font-bold text-3xl text-secondary">{title}</h1>
+      <h2 className="text-secondary">{subTitle}</h2>
+    </div>
+  );
+}
