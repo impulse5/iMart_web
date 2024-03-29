3f881b9873f74fc8dd17694e88815c07d5e82343
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-03-29 16:55:19 -0300

    [#14] add route and business address registration screen and form style

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../screens/Home';
 import TestComponents from '../screens/TestComponents';
 import { EnterpriseData } from '../screens/MarketRegister/EnterpriseData';
+import { EnterpriseAddress } from '../screens/MarketRegister/EnterpriseAddress';
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/test-components" element={<TestComponents />} />
       <Route path="/cadastre-se/dados-empresariais" element={<EnterpriseData />} />
+      <Route path="/cadastre-se/endereco" element={<EnterpriseAddress />} />
     </Routes>
   );
 }
