82f9e1774f7b49a7958142ff135d5a8ef700ecc7
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-03-29 17:19:25 -0300

    [#15] add route and business access registration screen and form style

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -2,12 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../screens/Home';
 import TestComponents from '../screens/TestComponents';
 import { EnterpriseData } from '../screens/MarketRegister/EnterpriseData';
+import { EnterpriseAccess } from '../screens/MarketRegister/EnterpriseAccess';
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/test-components" element={<TestComponents />} />
       <Route path="/cadastre-se/dados-empresariais" element={<EnterpriseData />} />
+
+      <Route path="/cadastre-se/acesso" element={<EnterpriseAccess/>} />
     </Routes>
   );
 }
---
+++ src/screens/MarketRegister/EnterpriseAccess.jsx
@@ -0,0 +1,55 @@
+import { Input } from '../../components/ui/Input/input';
+import { HeaderRegister } from '../../components/HeaderRegister';
+import { Button } from '../../components/ui/Button/button';
+import { SectionDescription } from '../../components/SectionDescription';
+import { Breadcrumb } from '../../components/ui/Breadcrumb/breadcrumb';
+export function EnterpriseAccess() {
+  const breadcrumbItems = [
+    { text: 'Dados empresariais', link: '/cadastre-se/dados-empresariais', current: false },
+    { text: 'Endereço', link: '/cadastre-se/endereco', current: false },
+    { text: 'Acesso', current: true },
+  ];
+  return (
+    <main className="bg-primary grid grid-cols-custom w-full h-screen overflow-hidden">
+      <div className="flex justify-center items-center ">
+        <div className="w-[27rem]">
+          <SectionDescription
+            title="Estamos quase finalizando!"
+            subTitle="Precisamos de uma conta para você acessar a plataforma da sua empresa!"
+          />
+        </div>
+      </div>
+      <div>
+        <form className="h-full bg-secondary rounded-form">
+          <div className="flex items-center flex-col">
+            <HeaderRegister subTitle="Gerencie de forma inteligente o seu mercado com a gente!" />
+          </div>
+          <div className="flex flex-col mt-10 mx-16 gap-4">
+            <div>
+              <Input type="text" placeholder="Jonh Doe" label="Nome" />
+            </div>
+            <div>
+              <Input placeholder="seu@email.com" type="email" label="Email" />
+            </div>
+            <div>
+              <Input placeholder="*********" type="password" label="Senha" />
+            </div>
+            <div>
+              <Input placeholder="*********" type="password" label="Confirme sua senha" />
+            </div>
+          </div>
+          <div className="my-10 mx-16 flex flex-col items-center">
+            <div className="mt-4 mb-1 text-nowrap">
+              <Breadcrumb items={breadcrumbItems} />
+            </div>
+            <Button className="w-full">Proxíma etapa</Button>
+            <p className="mt-2 text-nowrap text-sm">
+              Sua empresa já foi cadastrada?{' '}
+              <span className="font-bold cursor-pointer hover:text-tertiary text-sm">Faça seu Cadastro</span>
+            </p>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+}
