1d2e651e85c0c1fc8e2d737fb5d860e523b71526
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-03-29 11:49:28 -0300

    [#4] add merchant registration route and screen and form style

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../screens/Home';
 import TestComponents from '../screens/TestComponents';
-
+import { MerchantRegistration } from '../screens/MerchantRegistration';
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/test-components" element={<TestComponents />} />
+      <Route path="merchant-registration" element={<MerchantRegistration />} />
     </Routes>
   );
 }
---
+++ src/screens/MerchantRegistration.jsx
@@ -0,0 +1,51 @@
+import { Input } from '../components/ui/Input/input';
+import { HeaderRegister } from '../components/HeaderRegister';
+import { Button } from '../components/ui/Button/button';
+
+export function MerchantRegistration() {
+  return (
+    <main className="bg-primary grid grid-cols-custom w-full h-screen">
+      <div className="flex justify-center items-center ">
+        <div className="w-[26rem]">
+          {/* <SectionDescription
+            title="Dados Empresariais"
+            subTitle="Que tal facilitar o seu gerenciamento de mercado e ainda alavancar suas vendas? Vem com a iMart! 🤩"
+          /> */}
+        </div>
+      </div>
+      <div>
+        <form className="h-full bg-secondary rounded-form">
+          <div className="flex items-center flex-col">
+            <HeaderRegister subTitle="Gerencie de forma inteligente o seu mercado com a gente!" />
+          </div>
+          <div className="flex flex-col mt-12 mx-16 gap-4">
+            <div>
+              <Input type="text" placeholder="Supermercado iMart LTDA" label="Nome da empresa" />
+            </div>
+            <div>
+              <Input placeholder="00.000.000/0000-00" type="text" label="CNPJ" />
+            </div>
+            <div>
+              <Input placeholder="+55 99 99999-9999" type="tel" label="Telefone" />
+            </div>
+          </div>
+          <div className="my-14 mx-16 flex flex-col items-center">
+            {/* Component Breadcrumb */}
+            <div className="mt-4 mb-1 text-nowrap">
+              <span className="font-bold">Dados Empresariais</span>
+              {' > '}
+              <span className="font-semibold">Endereço</span> {' > '}
+              <span className="font-semibold">Acesso</span>
+            </div>
+            <Button className="w-full">Proxíma etapa</Button>
+
+            <p className="mt-2 text-nowrap text-sm">
+              Sua empresa já foi cadastrada?{' '}
+              <span className="font-bold cursor-pointer hover:text-tertiary text-sm">Faça login aqui!</span>
+            </p>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+}
