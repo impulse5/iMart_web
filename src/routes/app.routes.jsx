face2e47ced7b7cd8623e4508e156fbce61842c7
Matheus Gois <matheus.gois@mercadodiferente.com.br>
2024-03-29 18:13:51 -0300

    fix: remove unecessary routes and add redirect component

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,17 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from '../screens/Home';
-import TestComponents from '../screens/TestComponents';
 import { EnterpriseData } from '../screens/MarketRegister/EnterpriseData';
 import { EnterpriseAccess } from '../screens/MarketRegister/EnterpriseAccess';
 import { EnterpriseAddress } from '../screens/MarketRegister/EnterpriseAddress';
+import { Redirect } from '@/components/Redirect';
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/test-components" element={<TestComponents />} />
+      <Route path="/" element={<Redirect route="/cadastre-se/dados-empresariais" />} />
       <Route path="/cadastre-se/dados-empresariais" element={<EnterpriseData />} />
-      <Route path="/cadastre-se/acesso" element={<EnterpriseAccess/>} />
+      <Route path="/cadastre-se/acesso" element={<EnterpriseAccess />} />
       <Route path="/cadastre-se/endereco" element={<EnterpriseAddress />} />
     </Routes>
   );
