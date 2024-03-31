06429d5cadf9f2d2e2eb6041828f45f153275107
Matheus Gois <matheus.gois@mercadodiferente.com.br>
2024-03-31 00:28:34 -0300

    [#97] Solve password length

--- src/screens/MarketRegister/EnterpriseAccess.jsx
+++ src/screens/MarketRegister/EnterpriseAccess.jsx
@@ -17,11 +17,11 @@ const EnterpriseAccessSchema = z.object({
   email: z
     .string()
     .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, { message: 'O email informado é inválido.' }),
-  password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/, {
+  password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
     message:
-      'A senha deve ter pelo menos 6 caracteres, uma letra maiúscula, uma letra minúscula, um caractere especial, e um número.',
+      'A senha deve ter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula, um caractere especial, e um número.',
   }),
-  password_confirmation: z.string().min(6),
+  password_confirmation: z.string().min(8),
 });
 
 export function EnterpriseAccess() {
