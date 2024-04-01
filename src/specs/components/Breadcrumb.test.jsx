5d50f0ee5f3fdb519ecdf572d87cb7251bf197f6
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-03-31 22:03:41 -0300

    fix test breadcrumb component

--- src/specs/components/Breadcrumb.test.jsx
+++ src/specs/components/Breadcrumb.test.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import {Breadcrumb} from '../../components/ui/Breadcrumb/breadcrumb';
+import { MemoryRouter } from 'react-router-dom';
+import { Breadcrumb } from '../../components/ui/Breadcrumb/breadcrumb';
 
-const breadcrumbItems = [
-    { text: 'Dados empresarias', link: '/', current: 'false' },
-    { text: 'Endereço', link: '/endereco', current: 'false' },
-    { text: 'Acesso', link: '/acesso', current: 'true' },
-  ];
-  
-  test('renders Breadcrumb component with correct items', () => {
-    const { getByText } = render(<Breadcrumb items={breadcrumbItems} />);
+describe('Breadcrumb Component', () => {
+  test('renders breadcrumb items correctly', () => {
+    const breadcrumbItems = [
+      { text: 'Dados empresariais', current: true },
+      { text: 'Endereço', current: false },
+      { text: 'Acesso', current: false },
+    ];
 
-    breadcrumbItems.forEach(item => {
-      const linkElement = getByText(item.text);
-      expect(linkElement).toBeInTheDocument();
-      expect(linkElement.getAttribute('href')).toBe(item.link);
+    const { getByText } = render(
+      <MemoryRouter>
+        <Breadcrumb items={breadcrumbItems} />
+      </MemoryRouter>
+    );
+
+    breadcrumbItems.forEach((item) => {
+      const breadcrumbItem = getByText(item.text);
+      expect(breadcrumbItem).toBeInTheDocument();
+      if (item.current) {
+        expect(breadcrumbItem).toHaveClass('transition-colors hover:text-foreground font-bold '); 
+      } else {
+        expect(breadcrumbItem).not.toHaveClass('transition-colors hover:text-foreground font-bold ');
+      }
     });
-  
-    const currentBreadcrumbItem = breadcrumbItems.find(item => item.current === 'true');
-    const activeItem = getByText(currentBreadcrumbItem.text);
-    expect(activeItem).toBeInTheDocument();
   });
+});
--- src/specs/components/Toast.test.jsx
+++ src/specs/components/Toast.test.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import {EnterpriseData} from '../../screens/MarketRegister/EnterpriseData'; 
-import { RegisterMarketProvider } from '../../contexts/RegisterMarketContext';// Substitua pelo caminho correto do seu componente
+import { RegisterMarketProvider } from '../../contexts/RegisterMarketContext';
 import { MemoryRouter } from 'react-router-dom';
-describe('Teste do Toast', () => {
-    test('Exibição do Toast de erro ao enviar formulário com campos vazios', async () => {
+describe('test toast', () => {
+    test('Toast displays error when submitting form with empty fields', async () => {
       const { getByTestId, getByText } = render(
         <MemoryRouter>
             <RegisterMarketProvider>
@@ -12,16 +12,10 @@ describe('Teste do Toast', () => {
           </RegisterMarketProvider>
         </MemoryRouter>
       );
-      
-      // Simula o envio do formulário com campos vazios
-      fireEvent.submit(getByTestId('button'));
-  
-      // Aguarda a exibição do toast de erro
+      fireEvent.submit(getByTestId('button')); 
       await waitFor(() => {
         expect(getByText('Erro no formulário')).toBeInTheDocument();
         expect(getByText('Preencha todos os campos obrigatórios!')).toBeInTheDocument();
       });
     });
-  
-    // Você pode adicionar mais testes para outros cenários, se necessário
   });
