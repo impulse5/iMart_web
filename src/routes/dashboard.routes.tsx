6472379c049347f0ea9b349c4f80938d42e21cb1
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-04-10 21:48:23 -0300

    [#140] add supplier screen

--- src/routes/dashboard.routes.tsx
+++ src/routes/dashboard.routes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { Sidebar } from '../screens/Dashboards/Sidebar'
 import { Dashboard } from '../screens/Dashboards/Dashboard'
 import { EmployeeDashboard } from '@/screens/Dashboards/EmployeeDashboard'
+import { SupplierDashboard } from '@/screens/Dashboards/SupplierDashboard'
 import { ProtectedRoutes } from './protected.routes'
 
 export function DashboardRoutes() {
@@ -12,6 +13,7 @@ export function DashboardRoutes() {
         <Route element={<ProtectedRoutes/>}>
           <Route index path="/dashboard" element={<Dashboard/>} />
           <Route path='/funcionarios' element={<EmployeeDashboard/>} />
+          <Route path='/fornecedores' element={<SupplierDashboard/>} />
         </Route>
       </Route>
     </Routes>
