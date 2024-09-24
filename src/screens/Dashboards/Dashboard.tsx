bc7912daa6253c8317fb4b03ace4e76f9c4a1da7
arthurwillame5 <arthurwillame2005@gmail.com>
2024-09-24 17:53:06 -0300

    feat: add sales chart by products

--- src/screens/Dashboards/Dashboard.tsx
+++ src/screens/Dashboards/Dashboard.tsx
@@ -9,50 +9,74 @@ import {
   } from "@/components/ui/chart"
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 
-  const chartData = [
-    { category: "Bebidas", sells: 10, fill: "var(--color-bebidas)" },
-    { category: "Carnes", sells: 3, fill: "var(--color-carnes)" },
-    { category: "Higiene Pessoal", sells: 5, fill: "var(--color-higiene)" },
-    { category: "Laticínios", sells: 7, fill: "var(--color-laticinios)" },
-    { category: "Padaria", sells: 9, fill: "var(--color-padaria)" },
-    { category: "Verduras", sells: 6, fill: "var(--color-verduras)" },
+  const sellByProducts = [
+    { category: "Bebidas", sells: 10, fill: "#2563eb" },
+    { category: "Carnes", sells: 3, fill: "#AF57DB" },
+    { category: "Higiene Pessoal", sells: 5, fill: "#2EB88A" },
+    { category: "Laticínios", sells: 7, fill: "#ffbb00" },
+    { category: "Padaria", sells: 9, fill: "#E88C30" },
+    { category: "Verduras", sells: 6, fill: "#b20505" },
 ];
 
-const chartConfig = {
+    const chartData = [
+        { product: "Alface Americana", sells: 20, fill: "#b20505" },
+        { product: "Carne Bovina Moída 500g", sells: 12, fill: "#AF57DB" },
+        { product: "Coca-Cola 350ml", sells: 33, fill: "#2563eb" },
+        { product: "Danone Yogurt Morango", sells: 24, fill: "#ffbb00" },
+        { product: "Pão Francês 100g", sells: 17, fill: "#04ea1b" },
+        { product: "Sabonete Dove 90g", sells: 10, fill: "#2EB88A" },
+    ];
+
+const chartConfigCategory = {
     sells: {
       label: "Vendas",
-        color: "#FFFFF",
     },
     bebidas: {
       label: "Bebidas",
-      color: "#2563eb",
     },
     carnes: {
       label: "Carnes",
-      color: "#AF57DB",
     },
     higiene: {
       label: "Higiene Pessoal",
-      color: "#2EB88A",
     },
     laticinios: {
       label: "Laticínios",
-      color: "#ffbb00",
     },
     padaria: {
       label: "Padaria",
-      color: "#E88C30",
     },
     verduras: {
       label: "Verduras",
-      color: "#b20505",
+    },
+  } satisfies ChartConfig;
+
+  const chartConfig = {
+    sells: {
+      label: "Vendas",
+    },
+    alface: {
+      label: "Alface Americana",
+    },
+    carne: {
+      label: "Carne Bovina Moída 500g",
+    },
+    cocaCola: {
+      label: "Coca-Cola 350ml",
+    },
+    danone: {
+      label: "Danone Yogurt Morango",
+    },
+    pao: {
+      label: "Pão Francês 100g",
+    },
+    sabonete: {
+      label: "Sabonete Dove 90g",
     },
   } satisfies ChartConfig;
 
 export function Dashboard() {
-   
   return (
-
         <main className="px-10 pt-4 w-full h-screen bg-primary rounded-dashboard">
             <DashboardHeader title="Dashboard"/>
             <section className="grid grid-cols-3 gap-4 mt-10">
@@ -84,7 +108,7 @@ export function Dashboard() {
                     </CardHeader>
                     <CardContent className="flex-1 pb-0">
                         <ChartContainer
-                            config={chartConfig}
+                            config={chartConfigCategory}
                             className="mx-auto aspect-square max-h-[250px] "
                         >
                             <PieChart>
@@ -93,7 +117,7 @@ export function Dashboard() {
                                     content={<ChartTooltipContent hideLabel />}
                                 />
                                 <Pie
-                                    data={chartData}
+                                    data={sellByProducts}
                                     dataKey="sells" 
                                     nameKey="category" 
                                     innerRadius={60}
@@ -102,7 +126,7 @@ export function Dashboard() {
                                     <Label
                                         content={({ viewBox }) => {
                                             if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                                                const totalSells = chartData.reduce((acc, item) => acc + item.sells, 0);
+                                                const totalSells = sellByProducts.reduce((acc, item) => acc + item.sells, 0);
                                                 return (
                                                     <text
                                                         x={viewBox.cx}
@@ -134,20 +158,63 @@ export function Dashboard() {
                         </ChartContainer>
                     </CardContent>
                 </Card>
-                <aside className="col-span-1 bg-tertiary rounded-lg px-8 py-4 overflow-auto max-h-[300px]">
-                    <h1 className="text-xl font-semibold text-neutral-400">Transações</h1>
-                    <div className="flex flex-col mt-6 gap-3 text-left items-left ">
-                    {Array.from({ length: 10}).map((_, index) => (
-                        <div key={index} className="flex gap-5 ml-2 items-center">
-                        <TicketCheck className="text-neutral-400"/>
-                        <div>
-                            <h1 className="text-base font-semibold">Macaco de pelúcia</h1>
-                            <span className="font-normal text-sm text-neutral-400">Pagamento via pix</span>
-                        </div>
-                    </div>
-                    ))}
-                    </div>
-                </aside>
+                <Card className="flex flex-col bg-tertiary border-none">
+                    <CardHeader className="items-center pb-0">
+                        <CardTitle className="text-white">Vendas por produtos</CardTitle>
+                        <CardDescription>Janeiro - Junho 2024</CardDescription>
+                    </CardHeader>
+                    <CardContent className="flex-1 pb-0">
+                        <ChartContainer
+                            config={chartConfig}
+                            className="mx-auto aspect-square max-h-[250px]"
+                        >
+                            <PieChart>
+                                <ChartTooltip
+                                    cursor={false}
+                                    content={<ChartTooltipContent hideLabel />}
+                                />
+                                <Pie
+                                    data={chartData}
+                                    dataKey="sells" 
+                                    nameKey="product" 
+                                    innerRadius={60}
+                                    strokeWidth={5}
+                                >
+                                    <Label
+                                        content={({ viewBox }) => {
+                                            if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                                                const totalSells = chartData.reduce((acc, item) => acc + item.sells, 0); // Cálculo do total de vendas
+                                                return (
+                                                    <text
+                                                        x={viewBox.cx}
+                                                        y={viewBox.cy}
+                                                        textAnchor="middle"
+                                                        dominantBaseline="middle"
+                                                    >
+                                                        <tspan
+                                                            x={viewBox.cx}
+                                                            y={viewBox.cy}
+                                                            className="fill-white text-3xl font-bold"
+                                                        >
+                                                            {totalSells.toLocaleString()}
+                                                        </tspan>
+                                                        <tspan
+                                                            x={viewBox.cx}
+                                                            y={(viewBox.cy || 0) + 24}
+                                                            className="fill-muted-foreground"
+                                                        >
+                                                            Vendas
+                                                        </tspan>
+                                                    </text>
+                                                );
+                                            }
+                                        }}
+                                    />
+                                </Pie>
+                            </PieChart>
+                        </ChartContainer>
+                    </CardContent>
+                </Card>
             </section>
         </main>
   );
