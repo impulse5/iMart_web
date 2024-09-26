5d8f7e2dcb4cc372387ecd3ad25a8b7866a52679
gabrielfel1x <gabrielfelxx@gmail.com>
2024-09-26 15:24:13 -0300

    api routes

--- src/cable.ts
+++ src/cable.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 import { createConsumer } from "@rails/actioncable";
 
-const cable = createConsumer("https://1eea-2804-29b8-504e-a00-cd63-b72-17d7-d255.ngrok-free.app/cable");
+const cable = createConsumer("https://91de-200-129-62-72.ngrok-free.app/cable");
 
 export default cable;
